7a0d97363ff9d570cdfbc51385a01f7595b079f4
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-01 12:09:18 +0200

    add userService

---
+++ src/_services/userService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { global } from "../_global/global";
+
+const userService = {}
+
+userService.getAllPatients = async (token) =>{
+    const config = {
+        headers: {
+            Autorization: `Bearer${token}`
+        },
+    };
+    return (await axios.get(global.BASE_URL + "/usuarios", config)).data
+}
