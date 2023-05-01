d664e73e47aa7b351338742d5c43c7a7b32ce3ef
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-01 11:51:10 +0200

    add service for login

---
+++ src/_services/authService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { global } from "../_global/global";
+
+const authService = {};
+
+authService.login = async (credentials) => {
+  const body = {
+    email: credentials.email,
+    password: credentials.password,
+  };
+
+  return (await axios.post(global.BASE_URL + "auth/login", body)).data;
+};
+
+export default authService
