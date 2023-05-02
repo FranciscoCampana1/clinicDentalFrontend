d3703bb5245a97b9a5e704058b0f4285b2be28f2
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-02 17:24:16 +0200

    install react-redux and corrected App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from './components/header/Header';
+import Home from './containers/home/Home';
+import AllUsers from './containers/users/all-users/AllUsers';
+import Login from './containers/login/Login';
+import Register from './containers/register/Register';
+import UserProfile from './containers/users/profileUser/ProfileUser';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+          <Header />
+          <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/users" element={<AllUsers />} />
+             <Route path="/profile" element={<UserProfile />} />
+             <Route path="/login" element={<Login />} />
+             <Route path="/register" element={<Register />} />
+          </Routes>
+          {/* Footer */}
+       </BrowserRouter>
+  </>
   )
 }
 
