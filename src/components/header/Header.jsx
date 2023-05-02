71357d9ac0a0244cc53b3b7dd12893ef6d913295
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-02 17:26:26 +0200

    corrected Header.jsx and Header.scss

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Header.scss"
+import { Link, NavLink } from "react-router-dom";
+import "./Header.scss";
 
 export default function Header() {
-  return <>
-  <div className="Header">
-    <nav>
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-        </div>
-        <div>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
-            <Link >Logout</Link>
-        </div>
-    </nav>
-  </div>
-  
-  
-  
-  
-  
-  </>;
+  return (
+    <div className="Header">
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/courses">Courses</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/admin">Admin</NavLink>
+          </li>
+        </ul>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li className="nav-item dropdown">
+            <a>User Name</a>
+            <ul className="dropdown-menu">
+              <li className="dropdown-item">
+                <NavLink to="/profile">Profile</NavLink>
+              </li>
+              <li className="dropdown-item">
+                 <a>Logout</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
