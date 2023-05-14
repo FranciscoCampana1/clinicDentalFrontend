f2f47fb982815d6bd61d8d4470e9c8039a14d4ff
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-14 11:38:53 +0200

    change dates in service registerUser

--- src/_services/authService.js
+++ src/_services/authService.js
@@ -19,7 +19,6 @@ authService.registerUser = async (data) => {
   const body = {
     nombre: data.nombre,
     apellidos: data.apellidos,
-    edad: data.edad,
     email: data.email,
     fecha_de_nacimiento: data.fecha_de_nacimiento,
     telefono: data.telefono,
